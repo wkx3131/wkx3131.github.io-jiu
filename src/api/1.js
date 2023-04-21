0978acc2ab626dce7e87faeea95dbe8ee1e8ce93
wangkaixuan <wangkaixuan@zjkeyou.com>
2023-04-21 09:41:11 +0800

    open

--- src/api/1.js
+++
@@ -1,9 +0,0 @@
-// 封装api接口
-import axios1 from './request'
-export const aAPI = function(a){//按需导出函数
-    return axios1.get('/xx',{params:{a}})
-}
-// import {aAPI} from '@/api/1.js' 其他页面这样调用，{}是谁就导出谁
-// 这就是export，export default的区别
-
-// 到时候，需要的页面就调用这个js,aAPI(a),参数写在页面的data
--- src/api/request.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-const axios1 = axios.create({
- baseURL:'http://localhost:8080/'
-})
-export default axios1
-// 这部分是指定api头
